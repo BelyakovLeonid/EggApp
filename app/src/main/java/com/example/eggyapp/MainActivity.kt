cdb437c2e9ca828cfc88c47673029a5e4741cb12
belyakov <leonid.belyakov@magenta-technology.com>
2020-07-17 07:43:46 +0400

    initial commit

---
+++ app/src/main/java/com/example/eggyapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.eggyapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.f_egg_setup)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setFullScreenMode()
+    }
+
+    private fun setFullScreenMode() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
