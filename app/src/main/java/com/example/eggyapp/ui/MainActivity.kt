e917d60da53d630fd6a6e1263215c09026e83a53
belyakov <leonid.belyakov@magenta-technology.com>
2020-07-23 22:28:24 +0400

    implement ask confirm before exit

--- app/src/main/java/com/example/eggyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/eggyapp/ui/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.eggyapp.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.example.eggyapp.R
+import com.example.eggyapp.utils.isShowing
+import com.example.eggyapp.utils.showToast
 
 class MainActivity : AppCompatActivity() {
+
+    private var toast: Toast? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.a_main)
+        setupBackPressedListener()
+    }
+
+    private fun setupBackPressedListener() {
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (toast.isShowing) {
+                    finish()
+                } else {
+                    toast = showToast(getString(R.string.exit_hint))
+                }
+            }
+        })
     }
 
     override fun onStart() {
--- app/src/main/java/com/example/eggyapp/utils/ViewUtils.kt
+++ app/src/main/java/com/example/eggyapp/utils/ViewUtils.kt
@@ -1,5 +1,9 @@
 package com.example.eggyapp.utils
 
 import android.view.View
+import android.widget.Toast
 
 fun View.dpToPx(dp: Int) = resources.displayMetrics.density * dp
+
+val Toast?.isShowing
+    get() = this?.view?.isShown == true
