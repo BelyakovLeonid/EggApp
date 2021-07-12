e681c7fc57ab0f99518321a8fb16daf6cbb025b8
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-07-12 22:58:13 +0400

    Переработан deprecated fullscreen метод

--- app/src/main/java/com/example/eggyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/eggyapp/ui/MainActivity.kt
@@ -5,19 +5,25 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.eggyapp.R
+import com.example.eggyapp.databinding.AMainBinding
 import com.example.eggyapp.timer.TimerService
 import com.example.eggyapp.utils.isShowing
 import com.example.eggyapp.utils.showToast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.a_main) {
 
     private var toast: Toast? = null
+    private val binding by viewBinding(AMainBinding::bind)
+
     private var timerBinder: TimerService.TimerBinder? = null
     private val connection = object : ServiceConnection {
         override fun onServiceDisconnected(name: ComponentName?) {
@@ -36,7 +42,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.a_main)
         setupBackPressedListener()
         startTimerService()
     }
@@ -68,9 +73,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setFullScreenMode() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.mainContainer).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
