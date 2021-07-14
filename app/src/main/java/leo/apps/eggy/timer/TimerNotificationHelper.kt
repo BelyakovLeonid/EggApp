ba8705040e8f24c9187df4f7f35de673c1d542e0
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-07-14 15:33:08 +0400

    вынес работу с нотификациями из сервиса в отдельныйй класс

---
+++ app/src/main/java/leo/apps/eggy/timer/TimerNotificationHelper.kt
@@ -0,0 +1,152 @@
+package leo.apps.eggy.timer
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
+import androidx.core.graphics.drawable.IconCompat
+import androidx.navigation.NavDeepLinkBuilder
+import leo.apps.eggy.R
+import leo.apps.eggy.base.data.model.SetupType
+import leo.apps.eggy.base.utils.getBitmap
+
+
+class TimerNotificationHelper(
+    private val context: Context
+) {
+
+    private val manager: NotificationManager? = context.getSystemService()
+    private val intent = setupIntent()
+    private val action = setupAction()
+
+    private var eggType: SetupType? = null
+
+    private val notificationIcon: Bitmap
+        get() = when (eggType) {
+            SetupType.SOFT -> context.getBitmap(R.drawable.egg_soft)
+            SetupType.MEDIUM -> context.getBitmap(R.drawable.egg_medium)
+            else -> context.getBitmap(R.drawable.egg_hard)
+        }
+
+    private val notificationTitle: String
+        get() = when (eggType) {
+            SetupType.SOFT -> context.getString(R.string.timer_notif_soft)
+            SetupType.MEDIUM -> context.getString(R.string.timer_notif_medium)
+            else -> context.getString(R.string.timer_notif_hard)
+        }
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationProgressChannel()
+            createNotificationFinishChannel()
+        }
+    }
+
+    fun setEggType(type: SetupType){
+        eggType = type
+    }
+
+    fun cancelNotification(){
+        manager?.cancel(NOTIFICATION_ID)
+    }
+
+    fun createProgressNotification(
+        progress: Int,
+        text: String,
+    ): Notification {
+        return buildBaseNotification(NOTIF_PROGRESS_CHANNEL_ID, text)
+            .setCategory(Notification.CATEGORY_PROGRESS)
+            .setProgress(TimerService.MAX_PROGRESS, progress, false)
+            .addAction(action)
+            .build()
+    }
+
+    fun notifyFinish(){
+        val finishTitle = context.getString(R.string.timer_notif_finish_title)
+        val finishText = context.getString(R.string.timer_notif_finish_text)
+        val notification = buildBaseNotification(NOTIF_FINISH_CHANNEL_ID, finishText, finishTitle)
+            .setCategory(Notification.CATEGORY_EVENT)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .build()
+
+        manager?.notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun buildBaseNotification(
+        channelId: String,
+        text: String,
+        title: String? = notificationTitle
+    ) = NotificationCompat.Builder(context, channelId)
+        .setSmallIcon(R.drawable.ic_timer_gray)
+        .setLargeIcon(notificationIcon)
+        .setContentTitle(title)
+        .setContentText(text)
+        .setContentIntent(intent)
+
+    private fun setupIntent(): PendingIntent {
+        return NavDeepLinkBuilder(context)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.cookFragment)
+            .createPendingIntent()
+    }
+
+    private fun setupAction(): NotificationCompat.Action {
+        val actionIcon = IconCompat.createWithResource(context, R.drawable.ic_cancel)
+        val actionText = context.getString(R.string.cancel)
+
+        val intent = Intent(
+            context,
+            TimerService::class.java
+        ).putExtra(TimerService.ACTION_CANCEL, true)
+
+        val pendingIntent = PendingIntent.getService(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+        return NotificationCompat.Action.Builder(actionIcon, actionText, pendingIntent).build()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationProgressChannel() {
+        createChannel(
+            NOTIF_PROGRESS_CHANNEL_ID,
+            context.getString(R.string.timer_notif_progress_name),
+            context.getString(R.string.timer_notif_progress_description),
+            NotificationManager.IMPORTANCE_LOW
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationFinishChannel() {
+        createChannel(
+            NOTIF_FINISH_CHANNEL_ID,
+            context.getString(R.string.timer_notif_name),
+            context.getString(R.string.timer_notif_description),
+            NotificationManager.IMPORTANCE_HIGH
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel(id: String, name: String, desc: String, importance: Int) {
+        val channel = NotificationChannel(id, name, importance)
+        channel.setShowBadge(false)
+        channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        channel.description = desc
+        manager?.createNotificationChannel(channel)
+    }
+
+    companion object {
+        private const val NOTIF_PROGRESS_CHANNEL_ID = "progress_channel"
+        private const val NOTIF_FINISH_CHANNEL_ID = "finish_channel"
+        private const val NOTIFICATION_ID = 2
+    }
+}
--- app/src/main/java/leo/apps/eggy/timer/TimerService.kt
+++ app/src/main/java/leo/apps/eggy/timer/TimerService.kt
@@ -2,15 +2,9 @@ package leo.apps.eggy.timer
 
 import android.app.*
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Binder
-import android.os.Build
 import android.os.CountDownTimer
 import android.os.IBinder
-import androidx.core.app.NotificationCompat
-import androidx.core.content.getSystemService
-import androidx.core.graphics.drawable.IconCompat
-import androidx.navigation.NavDeepLinkBuilder
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,25 +12,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import leo.apps.eggy.R
 import leo.apps.eggy.base.data.model.SetupType
-import leo.apps.eggy.base.utils.getBitmap
 import leo.apps.eggy.base.utils.toTimerString
-
-private const val NOTIF_PROGRESS_CHANNEL_ID = "progress_channel"
-private const val NOTIF_FINISH_CHANNEL_ID = "finish_channel"
-private const val ACTION_CANCEL = "action_cancel"
-private const val MAX_PROGRESS = 1000
-private const val FOREGROUND_ID = 1
-private const val NOTIFICATION_ID = 2
 
 class TimerService : Service() {
 
-    private var manager: NotificationManager? = null
+    private var notificationHelper: TimerNotificationHelper? = null
     private val timerBinder = TimerBinder()
     private var timer: CountDownTimer? = null
-    private var action: NotificationCompat.Action? = null
-    private var intent: PendingIntent? = null
 
-    private var eggType: SetupType? = null
     private var millisInFuture: Long = 0
         set(value) {
             mutableProgress.value = 0f
@@ -50,27 +33,9 @@ class TimerService : Service() {
     private val finishEvent = Channel<Unit>(Channel.BUFFERED)
     private val cancelEvent = Channel<Unit>(Channel.BUFFERED)
 
-    private val notificationIcon: Bitmap
-        get() = when (eggType) {
-            SetupType.SOFT -> getBitmap(R.drawable.egg_soft)
-            SetupType.MEDIUM -> getBitmap(R.drawable.egg_medium)
-            else -> getBitmap(R.drawable.egg_hard)
-        }
-
-    private val notificationTitle: String
-        get() = when (eggType) {
-            SetupType.SOFT -> getString(R.string.timer_notif_soft)
-            SetupType.MEDIUM -> getString(R.string.timer_notif_medium)
-            else -> getString(R.string.timer_notif_hard)
-        }
-
     override fun onCreate() {
         super.onCreate()
-        manager = getSystemService()
-        action = setupAction()
-        intent = setupIntent()
-        createNotificationProgressChannel()
-        createNotificationFinishChannel()
+        notificationHelper = TimerNotificationHelper(this)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -80,7 +45,7 @@ class TimerService : Service() {
         return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
+    override fun onBind(intent: Intent?): IBinder {
         return timerBinder
     }
 
@@ -90,57 +55,14 @@ class TimerService : Service() {
         timer = null
     }
 
-    private fun setupAction(): NotificationCompat.Action {
-        val actionIcon = IconCompat.createWithResource(this, R.drawable.ic_cancel)
-        val actionText = getString(R.string.cancel)
-        val intent = Intent(this, TimerService::class.java).putExtra(ACTION_CANCEL, true)
-        val pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-        return NotificationCompat.Action.Builder(actionIcon, actionText, pendingIntent).build()
-    }
-
-    private fun setupIntent(): PendingIntent {
-        return NavDeepLinkBuilder(this)
-            .setGraph(R.navigation.nav_graph)
-            .setDestination(R.id.cookFragment)
-            .createPendingIntent()
-    }
-
-    private fun createNotificationProgressChannel() {
-        createChannel(
-            NOTIF_PROGRESS_CHANNEL_ID,
-            getString(R.string.timer_notif_progress_name),
-            getString(R.string.timer_notif_progress_description),
-            NotificationManager.IMPORTANCE_LOW
-        )
-    }
-
-    private fun createNotificationFinishChannel() {
-        createChannel(
-            NOTIF_FINISH_CHANNEL_ID,
-            getString(R.string.timer_notif_name),
-            getString(R.string.timer_notif_description),
-            NotificationManager.IMPORTANCE_HIGH
-        )
-    }
-
-    private fun createChannel(id: String, name: String, desc: String, importance: Int) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(id, name, importance)
-            channel.setShowBadge(false)
-            channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-            channel.description = desc
-            manager?.createNotificationChannel(channel)
-        }
-    }
-
     private fun startTimer() {
-        manager?.cancel(NOTIFICATION_ID)
+        notificationHelper?.cancelNotification()
 
         timer = object : CountDownTimer(millisInFuture, 10) {
             override fun onFinish() {
                 timer = null
                 stopForeground(true)
-                notifyFinish()
+                notificationHelper?.notifyFinish()
                 finishEvent.trySend(Unit)
                 mutableTimerText.value = millisInFuture.toTimerString()
             }
@@ -156,39 +78,14 @@ class TimerService : Service() {
         timer?.start()
     }
 
-    private fun notifyFinish() {
-        val finishTitle = getString(R.string.timer_notif_finish_title)
-        val finishText = getString(R.string.timer_notif_finish_text)
-        val notification = buildBaseNotification(NOTIF_FINISH_CHANNEL_ID, finishText, finishTitle)
-            .setCategory(Notification.CATEGORY_EVENT)
-            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-            .build()
-
-        manager?.notify(NOTIFICATION_ID, notification)
-    }
-
     private fun notifyProgress(progress: Float, timerString: String) {
-        val currentProgress = (progress * MAX_PROGRESS).toInt()
-        val contentText = getString(R.string.timer_notif_subtitle, timerString)
-        val notification = buildBaseNotification(NOTIF_PROGRESS_CHANNEL_ID, contentText)
-            .setCategory(Notification.CATEGORY_PROGRESS)
-            .setProgress(MAX_PROGRESS, currentProgress, false)
-            .addAction(action)
-            .build()
+        val notification = notificationHelper?.createProgressNotification(
+            progress = (progress * MAX_PROGRESS).toInt(),
+            text = getString(R.string.timer_notif_subtitle, timerString),
+        )
 
         startForeground(FOREGROUND_ID, notification)
     }
-
-    private fun buildBaseNotification(
-        channelId: String,
-        text: String,
-        title: String? = notificationTitle
-    ) = NotificationCompat.Builder(this, channelId)
-        .setSmallIcon(R.drawable.ic_timer_gray)
-        .setLargeIcon(notificationIcon)
-        .setContentTitle(title)
-        .setContentText(text)
-        .setContentIntent(intent)
 
     private fun stopTimer() {
         timer?.cancel()
@@ -223,7 +120,13 @@ class TimerService : Service() {
         }
 
         fun setType(type: SetupType) {
-            this@TimerService.eggType = type
+            this@TimerService.notificationHelper?.setEggType(type)
         }
+    }
+
+    companion object {
+        const val ACTION_CANCEL = "action_cancel"
+        const val MAX_PROGRESS = 1000
+        private const val FOREGROUND_ID = 1
     }
 }
