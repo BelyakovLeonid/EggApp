70a701221f01e1ec0c35565c2b497938b7d7343c
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-09-04 14:40:26 +0400

    fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         compileSdk = AppConfig.compileSdk
         minSdk = AppConfig.minSdk
         targetSdk = AppConfig.targetSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
